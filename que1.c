34f67e9f18e8f7e9a561ae13f9082026520ba878
JaydeepKaware <148674753+JaydeepKaware@users.noreply.github.com>
2023-10-22 11:38:04 +0530

    First File Upload

---
+++ que1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int height;
+int width;
+printf("height de do bhai : ");
+scanf("%i",&height);
+printf("width de do bhai : ");
+scanf("%i",&width);
+printf("total perimeter of rectangle is : %i",2*(height+width));
+return 0;
+}
